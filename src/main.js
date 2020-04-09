2ee3d146217d434c0fd4a0c894c3a1de898cb066
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-04-08 20:50:10 -0600

    JS hooked up to HTML

---
+++ src/main.js
@@ -0,0 +1,9 @@
+var menuButton = document.querySelector('.hamburger-menu');
+console.log(menuButton);
+console.log('loaded')
+
+// menuButton.addEventListener('click', showMobileMenu);
+// 
+// showMobileMenu() {
+//   window.alert('hey');
+// }
