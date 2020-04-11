5a0fe3c0ed645d6ca48e64e1fc0ef97a3629e462
Amy Karnaze <akarnaze@gmail.com>
2020-04-11 09:45:57 -0600

    Add class idea

---
+++ src/idea.js
@@ -0,0 +1,27 @@
+class Idea {
+  constructor(title, body) {
+    this.id = '';
+    this.title = title;
+    this.body = body;
+    this.star = true;
+
+  }
+
+// should only have one job which is to save the instance to storage
+  saveToStorage() {
+
+  }
+
+  deleteFromStorage() {
+
+  }
+
+// should be able to update the idea’s title, body, or starred state
+  updateIdea() {
+
+  }
+
+}
+
+
+// Idea methods must include, but are not limited to:
