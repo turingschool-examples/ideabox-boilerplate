bd66acea61e2b15f9ac63aaab404ad5235e004f8
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-04-13 15:29:54 -0600

    Delete button going and local storage enabled

--- src/idea.js
+++ src/idea.js
@@ -1,6 +1,6 @@
 class Idea {
   constructor(title, body, star) {
-    this.id = this.id = Date.now();
+    this.id = Date.now();
     this.title = title;
     this.body = body;
     this.star = undefined ? true : star;
