28c500c35356f74bdf64e1bb5b357efc9d4f074c
Charles Wefso <cwefso@gmail.com>
2020-04-13 13:19:13 -0600

    fixes spacing and indentation on main.js

--- src/main.js
+++ src/main.js
@@ -18,14 +18,14 @@ var savedIdeas = [];
 
 function showMobileMenu() {
   mobileMenu.classList.add('purple-4');
-  mobileMenuBody.classList.add('menu-body-mobile')
+  mobileMenuBody.classList.add('menu-body-mobile');
   menuButton.classList.add('hide');
   menuCloseButton.classList.remove('hide');
 }
 
 function closeMenu() {
   mobileMenu.classList.remove('purple-4');
-  mobileMenuBody.classList.remove('menu-body-mobile')
+  mobileMenuBody.classList.remove('menu-body-mobile');
   menuButton.classList.remove('hide');
   menuCloseButton.classList.add('hide');
 }
@@ -39,7 +39,7 @@ function saveIdea(event) {
 }
 
 function verifyForm() {
-  if(userNewTitle.value && userNewBody.value) {
+  if (userNewTitle.value && userNewBody.value) {
     saveIdeaButton.disabled = false;
   } else {
     saveIdeaButton.disabled = true;
@@ -57,24 +57,24 @@ function clearFields() {
 }
 
 function showUsersIdeaCard() {
-  if(savedIdeas.length) {
+  if (savedIdeas.length) {
     ideaGallery.innerHTML = "";
-    for(var i=0; i < savedIdeas.length; i++) {
+    for (var i=0; i < savedIdeas.length; i++) {
       var ideaCardTemplate =
-      `<section class="box">
-      <section class="card-top">
-      <input type="image" src="assets/star-active.svg" name="star-active" class="star-active" id="star-active" />
-      <input type="image" src="assets/delete.svg" name="delete" class="delete" id="delete" align="right"/>
-      </section>
-      <section class="card-body">
-      <p class= "card-header">${savedIdeas[i].title}</p>
-      <p class= "card-text">${savedIdeas[i].body}</p>
-      </section>
-      <section class="card-bottom">
-      <input type="image" src="assets/comment.svg" name="comment" class="comment" id="comment" align="left"/>
-      <p class= "comment-text">Comment</p>
-      </section>
-      </section>`;
+        `<section class="box">
+        <section class="card-top">
+        <input type="image" src="assets/star-active.svg" name="star-active" class="star-active" id="star-active" />
+        <input type="image" src="assets/delete.svg" name="delete" class="delete" id="delete" align="right"/>
+        </section>
+        <section class="card-body">
+        <p class= "card-header">${savedIdeas[i].title}</p>
+        <p class= "card-text">${savedIdeas[i].body}</p>
+        </section>
+        <section class="card-bottom">
+        <input type="image" src="assets/comment.svg" name="comment" class="comment" id="comment" align="left"/>
+        <p class= "comment-text">Comment</p>
+        </section>
+        </section>`;
       ideaGallery.insertAdjacentHTML('afterbegin', ideaCardTemplate);
     }
   }
