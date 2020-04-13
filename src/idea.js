a22ccee0390b36eb6581673f586e6db03e64440a
Charles Wefso <cwefso@gmail.com>
2020-04-13 13:12:55 -0600

    removed storage functions from idea.js

--- src/idea.js
+++ src/idea.js
@@ -5,13 +5,4 @@ class Idea {
     this.body = body;
     this.star = undefined ? true : star;
   }
-  saveToStorage() {
-    //should only have one job which is to save the instance to storage
-  }
-  deleteFromStorage() {
-
-  }
-  updateIdea() {
-    //should be able to update the idea’s title, body, or starred state
-  }
 }
