01472e0d5126d85cfb6d0cf7d8b1cc1f9eb75c3d
Horacio Borrego <hborrego365@gmail.com>
2020-04-13 20:36:35 -0600

    Refactored mini menu display function and idea validation function

--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
-var burgerButton = document.querySelector(".hamburger-img")
-var body = document.querySelector(".purple-1")
-var showIdeas = document.querySelector(".show-starred")
-var ideas = document.querySelector(".ideas")
+var burgerButton = document.querySelector(".hamburger-img");
+var body = document.querySelector(".purple-1");
+var showIdeas = document.querySelector(".show-starred");
+var ideas = document.querySelector(".ideas");
 var menuItems = document.querySelector('.menu-items');
-var hamburgerImg = document.querySelector('.hamburger-img')
 var miniMenuXButton = document.querySelector('.mini-menu-x-button');
 var miniMenuX = document.querySelector('.mini-menu-x');
 var miniMenu = document.querySelector('.mini-menu');
 var saveButton = document.querySelector('.save');
 var ideaTitleInput = document.querySelector('.input-title');
 var ideaBodyInput = document.querySelector('.input-body');
-var ideaCards = document.querySelector('.idea-cards')
+var ideaCards = document.querySelector('.idea-cards');
+var inactiveStar = document.querySelector('.inactive-star');
 var list = []
 
-body.addEventListener("click", buttonHandler);
-ideaTitleInput.addEventListener('keyup', enableSaveButton);
-ideaBodyInput.addEventListener('keyup', enableSaveButton);
+body.addEventListener("click",buttonHandler);
+
+ideaTitleInput.addEventListener('keyup', validateInputs);
+ideaBodyInput.addEventListener('keyup', validateInputs);
+ideaCards.addEventListener('click', function(event) {
+  console.log(event);
+})
 
 function buttonHandler(event) {
   if(event.target === showIdeas){
@@ -25,39 +29,29 @@ function buttonHandler(event) {
   } else if (event.target === miniMenuX) {
     hideStarredIdeasMenu();
   } else if (event.target === saveButton) {
-    validateInputs();
+    saveNewIdea()
+    
   }
 }
 
 function displayStarredIdeasMenu() {
-  menuItems.style.display = "flex";
-  miniMenuXButton.style.display = 'flex';
-  hamburgerImg.style.display = 'none';
-  burgerButton.style.display = 'none';
+  var menu = document.querySelector('.menu');
+  menu.classList.add('menu-open');
+
 }
 
 function hideStarredIdeasMenu() {
-  menuItems.style.display = 'none';
-  hamburgerImg.style.display = 'flex';
-  burgerButton.style.display = 'flex';
+  var menu = document.querySelector('.menu');
+  menu.classList.remove('menu-open');
+
 }
 
-function validateInputs(){
-  if (ideaTitleInput.value !== "" && ideaBodyInput.value !== ""){
-    enableSaveButton();
-    saveNewIdea()
-  }
-}
-
-function enableSaveButton() {
-  if (ideaTitleInput.value !== "" && ideaBodyInput.value !== ""){
-    saveButton.disabled = false;
-  } else {
-    saveButton.disabled = true;
-  }
+function validateInputs() {
+  saveButton.disabled = ideaTitleInput.value === "" || ideaBodyInput.value === "";
 }
 
 function saveNewIdea() {
+  saveButton.disabled = true;
   var newIdea = new Idea(ideaTitleInput.value, ideaBodyInput.value);
   ideaTitleInput.value = ""
   ideaBodyInput.value = ""
@@ -70,20 +64,21 @@ function displayIdeaCard() {
   for (var i = 0; i < list.length; i++){
     var currentIdea = list[i]
     var currentIdeaDetails =  `<section class="idea-card-unit" id='${currentIdea.id}'>
-            <section class="idea-box-top">
-              <button class="idea-box-top-buttons"><img src="assets/star.svg" alt="Orange Active Star Icon"></button>
-              <button class="idea-box-top-buttons"><img src="assets/delete.svg" alt=" X delete"></button>
-            </section>
-            <section class="idea-box-mid">
-              <h3>${currentIdea.title}</h3>
-              <p>${currentIdea.body}</p>
-            </section>
-            <section class="idea-box-bottom">
-              <button class="idea-box-bottom-button"><img src="assets/comment.svg" alt="Plus Icon add a comment"></button>
-              <p>Comment</p>
-            </section>
-          </section>`
-          ideaCards.insertAdjacentHTML('afterbegin', currentIdeaDetails)
-
+    <section class="idea-box-top">
+    <button class="idea-box-top-buttons inactive-star"><img src="assets/star.svg" alt="Inactive Star Icon"></button>
+    <button class="idea-box-top-buttons"><img src="assets/delete.svg" alt=" X delete"></button>
+    </section>
+    <section class="idea-box-mid">
+    <h3>${currentIdea.title}</h3>
+    <p>${currentIdea.body}</p>
+    </section>
+    <section class="idea-box-bottom">
+    <button class="idea-box-bottom-button"><img src="assets/comment.svg" alt="Plus Icon add a comment"></button>
+    <p>Comment</p>
+    </section>
+    </section>`
+    ideaCards.insertAdjacentHTML('afterbegin', currentIdeaDetails)
+    
   }
 }
+
