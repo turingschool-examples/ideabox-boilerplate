efca891c55d1567717ccc0cc9c27c1e7089bf8c4
Tim Keresey <tim.keresey@gmail.com>
2020-07-25 10:43:00 -0600

    add functionality to save button that adds input form values to cards

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ var saveButton = document.querySelector('.save-button');
 var titleInput = document.querySelector('.title-input');
 var bodyInput = document.querySelector('.body-input');
 var ideaFormSection = document.querySelector('.idea-form');
+var cardTitle = document.querySelector('.card-title');
+var bodyText = document.querySelector('.body-text');
 
 function toggleHidden() {
   menuIcon.classList.toggle("hidden");
@@ -12,9 +14,21 @@ function toggleHidden() {
   dropDownMenu.classList.toggle("hidden");
 }
 
+saveButton.addEventListener('click', formValidation);
+
+// function createCard() {
+//
+// }
+
+
+function addText() {
+  cardTitle.innerText = titleInput.value;
+  bodyText.innerText = bodyInput.value;
+}
+
 
 function formValidation() {
-  if (titleInput.value !=== '' && bodyInput.value !== '') {
-
+  if (titleInput.value !== '' && bodyInput.value !== '') {
+    addText();
   }
 }
