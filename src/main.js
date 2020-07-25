f7529940098a3c45a5e0ce5e17c51b21639b559d
jobbot <joevarela83@gmail.com>
2020-07-24 18:03:47 -0600

    Add opacity feature for displayOff and displayBack functions

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 var buttonMenu = document.querySelector('.menu-open');
 var menuOpen = document.querySelector('.open-menu');
 var menuClose = document.querySelector('.menu-close');
+var shadow = document.querySelector('.ideas');
 
 buttonMenu.addEventListener('click', toggleMenu);
 menuClose.addEventListener('click', displayBack);
@@ -10,13 +11,14 @@ function toggleMenu() {
   displayOff();
   menuClose.classList.remove('hidden');
   menuOpen.classList.remove('hidden');
+  shadow.classList.add('shadow');
 }
 
 function displayBack() {
   displayOff();
   buttonMenu.classList.remove('hidden');
+  shadow.classList.remove('shadow');
 }
-
 
 function displayOff() {
   buttonMenu.classList.add('hidden');
