e335ab82324c9cc61733b411e4ae2a5cdfd0f00b
Kelsie Besinger <kelsiebesinger@gmail.com>
2020-10-29 13:20:03 -0600

    Add idea.js file with a creation of an Idea class.

---
+++ src/idea.js
@@ -0,0 +1,21 @@
+class Idea {
+  constructor(title, body) {
+    this.id = Date.now();
+    this.title = title;
+    this.body = body;
+    this.star = false;
+  }
+
+//take in array as a parameter
+  saveToStorage() {
+
+  }
+
+  deleteFromStorage() {
+
+  }
+
+  updateIdea(title, body, star) {
+
+  }
+}
