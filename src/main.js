8835c26b4cd768825106ded3a8fd8536fd0cbf8c
Max Bregman <bregmanmax91@gmail.com>
2020-10-29 16:38:09 -0600

    can now favorite and unfavorite ideas which also changes the display of the star

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ var cardGrid = document.querySelector('.card-grid');
 var titleInput = document.querySelector('.title-input');
 var bodyInput = document.querySelector('.body-input');
 var ideas = [];
-var deleteButton = document.querySelector('#delete-card');
-
 
 //add event listeners here 🍊
 saveButton.addEventListener('click', makeNewIdeaCard);
-cardGrid.addEventListener('click', deleteIdea);
+cardGrid.addEventListener('click', upDateIdea);
 
 
 //add functions here 🍊
@@ -29,33 +27,49 @@ function upDateCardGrid() {
     for (var i = 0; i < ideas.length; i++) {
         newGrid +=
             `<section class="idea-card">
-            <div class="fav-delete-header">
-              <input type="image" id="star-${ideas[i].id}" class="star" src="./assets/star-active.svg">
-              <input type="image" id="delete-card-${ideas[i].id}" class="delete-card" src="./assets/delete.svg">
-            </div>
-            <div class="idea-content">
-              <p class="idea-title">${ideas[i].title}</p>
-              <p class="idea-body">${ideas[i].body}</p>
-            </div>
-            <div class="comment-strip">
-              <input type="image" id="idea-comment" name="comment" src="./assets/comment.svg">
-              <label for="comment">Comment</label>
-            </div>
-            </section>`;
+          <div class="fav-delete-header">
+            <input type="image" id="star-${ideas[i].id}" class="star" src="${testForStar(ideas[i])}">
+            <input type="image" id="delete-card-${ideas[i].id}" class="delete-card" src="./assets/delete.svg">
+          </div>
+          <div class="idea-content">
+            <p class="idea-title">${ideas[i].title}</p>
+            <p class="idea-body">${ideas[i].body}</p>
+          </div>
+          <div class="comment-strip">
+            <input type="image" id="idea-comment" name="comment" src="./assets/comment.svg">
+            <label for="comment">Comment</label>
+          </div>
+          </section>`;
     }
     cardGrid.innerHTML = newGrid;
-};
+}
 
-function deleteIdea() {
-
-    console.log(event.target.id);
+function upDateIdea() {
     if (event.target.id.includes('delete-card')) {
         for (var i = 0; i < ideas.length; i++) {
-            console.log('all the way in');
             if (`delete-card-${ideas[i].id}` === event.target.id) {
                 ideas.splice(i, 1);
             }
-            upDateCardGrid();
         }
+    } else if (event.target.id.includes('star')) {
+        for (var i = 0; i < ideas.length; i++) {
+
+            if (`star-${ideas[i].id}` === event.target.id) {
+                ideas[i].toggleStar();
+                console.log(ideas[i]);
+            }
+
+        }
+
     }
-};
+    upDateCardGrid();
+}
+
+
+function testForStar(idea) {
+    if (idea.star === true) {
+        return './assets/star-active.svg';
+    } else {
+        return './assets/star.svg';
+    }
+}
