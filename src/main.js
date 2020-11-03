29a6bf874cefd7c6dfc66e8bd10d069d30ae5d6e
Tashia Davis <tashiadavis10@gmail.com>
2020-11-03 09:56:51 -0700

    Add completed star local storage functionality

--- src/main.js
+++ src/main.js
@@ -27,7 +27,6 @@ cardDisplay.addEventListener("click", function(event) {
       if (parseInt(event.target.closest("article").id) === list[i].id) {
         deleteIdea(list[i].id);
         event.target.closest("article").remove();
-        // list[i].deleteFromStorage(list[i].id);
       }
     }
   }
@@ -49,23 +48,23 @@ function addToList(title, body) {
 
 function makeNewCard(event) {
   event.preventDefault();
-  addToList(inputTitle.value, inputBody.value); 
+  addToList(inputTitle.value, inputBody.value);
   refreshCard();
   clearInputs();
   checkInputs();
+  loadStars();
 };
 
 function refreshCard() {
   cardDisplay.innerHTML = "";
-  // retrieveFromLocalStorage();
   for (i = 0; i < list.length; i++) {
     cardDisplay.innerHTML +=
       `
     <article id="${list[i].id}">
       <div class="card-button-bar">
         <div class="favorite-box">
-          <button class="favorite-button" id="${list[i].id}"><img class="favorite-button" src="svg-files/star.svg"/></button>
-          <button class="favorite-button hidden" id="${list[i].id}"><img class="favorite-button" src="svg-files/star-active.svg"/></button>
+          <button class="favorite-button white-star" id="${list[i].id}"><img class="favorite-button" src="svg-files/star.svg"/></button>
+          <button class="favorite-button red-star hidden" id="${list[i].id}"><img class="favorite-button" src="svg-files/star-active.svg"/></button>
         </div>
         <div class="delete-box">
           <button class="delete-button delete-red" id="${list[i].id}"><img class="delete-img" src="svg-files/delete-active.svg"/></button>
@@ -98,16 +97,14 @@ function addStar(favoritedIdea) {
   sendToLocalStorage();
 }
 
-
 function fillStar(favoritedIdea) {
   var favoriteButton = document.querySelectorAll('.favorite-button');
   for (var i = 0; i < favoriteButton.length; i++) {
-    if ((favoritedIdea.id === parseInt(favoriteButton[i].id)) && (favoritedIdea.star === true)) {
-      favoriteButton[i].classList.remove("hidden");
+    if (favoritedIdea.id === parseInt(favoriteButton[i].id)) {
+      favoriteButton[i].classList.toggle("hidden");
     }
   }
 }
-
 
 function deleteIdea(deleteCard) {
   var deleteButtonRed = document.querySelectorAll('.delete-red');
@@ -115,7 +112,8 @@ function deleteIdea(deleteCard) {
     if(deleteCard === parseInt(deleteButtonRed[i].id)) {
       list.splice(i, 1);
     }
-  } sendToLocalStorage();
+  }
+  sendToLocalStorage();
 };
 
 function sendToLocalStorage() {
@@ -129,9 +127,23 @@ function retrieveFromLocalStorage() {
   for (var i = 0; i < parsedObject.length; i++) {
     var newObject = new Idea(parsedObject[i].title, parsedObject[i].body, parsedObject[i].star, parsedObject[i].id);
     list.push(newObject);
-    fillStar(list[i]);
-  } 
+  }
   refreshCard();
+  loadStars();
+}
+
+function loadStars() {
+  var redStar = document.querySelectorAll('.red-star');
+  var whiteStar = document.querySelectorAll('.white-star');
+  for (var i = 0; i < list.length; i++) {
+    if (list[i].star === true) {
+      whiteStar[i].classList.add("hidden");
+      redStar[i].classList.remove("hidden");
+    } else {
+      whiteStar[i].classList.remove("hidden");
+      redStar[i].classList.add("hidden");
+    }
+  }
 }
 
 // function loadStars() {
@@ -139,6 +151,6 @@ function retrieveFromLocalStorage() {
 //   for (var i = 0; i < favoriteButton.length; i++) {
 //     if (list[i].star === true) {
 //       favoriteButton[i].classList.remove("hidden");
-//     } 
+//     }
 //   }
 // }
