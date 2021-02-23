5a5c96be8217615af399c7e79fa07cb088bee5d7
StevenTheBearded <>
2021-02-23 16:26:36 -0500

    Add data.js file

---
+++ data.js
@@ -0,0 +1,8 @@
+var ideasList = [
+  {id:1, title:idea#1, body: sometext#1, isStar:false},
+  {id:2, title:idea#2, body: sometext#2, isStar:false},
+  {id:3, title:idea#3, body: sometext#3, isStar:false},
+  {id:4, title:idea#4, body: sometext#4, isStar:false}
+  ];
+
+
