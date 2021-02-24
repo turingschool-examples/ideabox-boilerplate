4501b6980c01ab0245920a10731529d0e1d1d053
StevenTheBearded <>
2021-02-24 17:43:17 -0500

    Active Star Button

--- src/main.js
+++ src/main.js
@@ -4,12 +4,24 @@ var inputText = document.querySelector(".text-box");
 var formInformation = document.querySelector("form");
 var renderIdeaBox = document.querySelector("#populatedIdea");
 var saveButton = document.querySelector('#saveButton');
+var starIcon = document.querySelector("#starIcon")
 
 // Event Listeners
 formInformation.addEventListener("submit", submitNewIdea);
 window.addEventListener("load", loadWindow);
 title.addEventListener("input", enableButton);
 inputText.addEventListener("input", enableButton);
+// renderIdeaBox.addEventListener("click", switchStarIcon);
+
+
+renderIdeaBox.addEventListener('click', function(event) {
+  for (var i = 0; i < 3; i++){
+  if (event.target.id === 'starIcon') {
+    starIcon.src = "./assets/star-active.svg"
+    console.log("star shold be here")  }
+  }
+});
+
 
 // global variables
 var newIdea = [];
@@ -22,18 +34,19 @@ function submitNewIdea(event) {
   renderIdea();
   clearTextBoxes();
   disableButton();
+
   // formInputValidation();
 
   //single responsibility function...
-}
+};
 
 function createIdeaList() {
   newIdea = new Idea(title.value, inputText.value);
-}
+};
 
 function updateIdeaList() {
   ideaList.unshift(newIdea);
-}
+};
 
 function renderIdea() {
     var createList = "";
@@ -42,7 +55,7 @@ function renderIdea() {
     for (var i = 0; i < ideaList.length; i++) {
     createList += `<div class="idea-boxes">
       <div class="idea-box-header">
-        <img class="star-active-icon icon" src="./assets/star-active.svg"/>
+        <img class="star-icon icon" id="starIcon" src="./assets/star.svg"/>
         <img class="delete-icon icon" src="./assets/delete.svg"/>
       </div>
       <div class="comment-information">
@@ -54,24 +67,24 @@ function renderIdea() {
         <p class="comment-class">Comment</p>
       </div>
       </div>`
-    }
-    renderIdeaBox.innerHTML = createList
-}
+    };
+    renderIdeaBox.innerHTML = createList;
+};
 
 function clearTextBoxes() {
-  title.value = ""
-  inputText.value = ""
-}
+  title.value = "";
+  inputText.value = "";
+};
 
 function loadWindow(event) {
   event.preventDefault();
   disableButton();
   // disable button on load 
-}
+};
 
 function disableButton() {
   saveButton.disabled = true;
-}
+};
 
 function enableButton(event) {
   event.preventDefault();
@@ -80,9 +93,15 @@ function enableButton(event) {
   var monitorTextBoxInput = inputText.value;
 
   if (monitorTitleInput.length !== 0 && monitorTextBoxInput.length !== 0) {
-    saveButton.disabled = false
+    saveButton.disabled = false;
   }
   if (monitorTitleInput.length === 0 || monitorTextBoxInput.length === 0) {
-    saveButton.disabled = true
-  }
-}
+    saveButton.disabled = true;
+  };
+};
+
+// function switchStarIcon() {
+//   starIcon.src = "./assets/star-active.svg"
+//   console.log("star shold be here")
+// }
+
