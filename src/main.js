80c9176d26d8f873b5a3426850c830cc0262f842
Chris Cole <christopher.a.cole@gmail.com>
2023-02-24 14:38:14 -0800

    feat: add functionality for 'delete x button'

--- src/main.js
+++ src/main.js
@@ -1,40 +1,57 @@
+//====================================================
 // Query Selectors
+//====================================================
+
 var savedButton = document.querySelector(".save-btn")
 var titleInput = document.querySelector(".title-input")
 var bodyInput = document.querySelector(".body-input")
 var ideaContainer = document.querySelector(".idea-box-container")
 var form = document.querySelector(".form-container")
+var deleteButton = document.querySelector(".delete-x")
 
+//====================================================
 // Variables
+//====================================================
+
 var currentIdea
 var savedIdeas = []
+
+//====================================================
 // Event Listeners
+//====================================================
 
 window.addEventListener('load', pageLoad)
+
 savedButton.addEventListener('click', function(event){
   event.preventDefault()
   saveCard()
   clearInput()
   disableSaveButton()
 })
+
 form.addEventListener('keydown', toggleSaveBtn)
 
-// Functions
+deleteButton.addEventListener('click', deleteIdeaCard)
 
+ideaContainer.addEventListener('click', updateIdea)
+
+
+//====================================================
+// Functions
+//====================================================
 
 function pageLoad() {
   disableSaveButton()
 }
 
-
 function createCard(){
   ideaContainer.innerHTML = ''
   for (var i = 0; i < savedIdeas.length; i++ ) {
     ideaContainer.innerHTML += 
-    `<section class="idea-card" id="${savedIdeas[i].id}">
+    `<section class="idea-card" id=${savedIdeas[i].id}>
       <section class="top-bar">
         <img class="star" src="assets/star.svg" alt="White Star">
-        <img class="delete-x" src="assets/delete.svg" alt="delete X">
+        <img class="delete-x" id=${savedIdeas[i].id} src="assets/delete.svg" alt="delete X">
       </section>
       <section class="card-description">
         <h3>${savedIdeas[i].title}</h3>
@@ -69,5 +86,20 @@ function toggleSaveBtn() {
   } else  {
     savedButton.disabled = true
   }
-
 }
+
+function deleteIdeaCard(event) {
+  for (var i = 0; i < savedIdeas.length; i++) {
+  if(parseInt(event.target.id) === savedIdeas[i].id) {
+    savedIdeas.splice(i, 1)
+    }
+  }
+  createCard()
+}
+
+function updateIdea(event) {
+  if(event.target.classList.contains('delete-x')) {
+  deleteIdeaCard(event)
+  }
+}
+
