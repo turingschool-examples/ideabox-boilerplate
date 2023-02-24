c3336ca31185399837f5d74a05149e44b80be9d0
Trey Marshall <marshall.trey92@yahoo.com>
2023-02-24 11:33:56 -0600

    fix: Correct wrapping issue for idea cards

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@
 var savedButton = document.querySelector(".save-btn")
 var titleInput = document.querySelector(".title-input")
 var bodyInput = document.querySelector(".body-input")
-var ideaContainer = document.querySelector(".idea-card")
-
+var ideaContainer = document.querySelector(".idea-box-container")
 
 // Variables
-  var currentIdea
-  var savedIdeas = [];
+var currentIdea
+var savedIdeas = []
 // Event Listeners
 
 savedButton.addEventListener('click', function(event){
@@ -20,24 +19,25 @@ saveCard()
 
 function createCard(){
   ideaContainer.innerHTML = ''
-  for (var i = 0; i < savedIdeas.length; i++ )
-  ideaContainer.innerHTML += 
-  // <section class="idea-card" id="dateNow">
-    `<section class="top-bar">
-      <img class="star" src="assets/star.svg" alt="White Star">
-      <img class="delete-x" src="assets/delete.svg" alt="delete X">
-    </section>
-    <section class="card-description">
-      <h3>${savedIdeas[i].title}</h3>
-      <p>${savedIdeas[i].body}</p>
-    </section>
-    <section class="bottom-bar">
-      <img class="comment-img" src="assets/comment.svg" alt="comment-img">
-      <h4>Comment</h4>
+  for (var i = 0; i < savedIdeas.length; i++ ) {
+    ideaContainer.innerHTML += 
+    `<section class="idea-card" id="${savedIdeas[i].id}">
+      <section class="top-bar">
+        <img class="star" src="assets/star.svg" alt="White Star">
+        <img class="delete-x" src="assets/delete.svg" alt="delete X">
+      </section>
+      <section class="card-description">
+        <h3>${savedIdeas[i].title}</h3>
+        <p>${savedIdeas[i].body}</p>
+      </section>
+      <section class="bottom-bar">
+        <img class="comment-img" src="assets/comment.svg" alt="comment-img">
+        <h4>Comment</h4>
+      </section>
     </section>`
-  // </section>
-
+  }
 }
+
 function saveCard(){
   currentIdea = new Idea(titleInput.value, bodyInput.value)
   savedIdeas.push(currentIdea);
